c83b98596a6dd36ecd05c3ed61135d4ac124680b
root <adib.grouz@gmail.com>
2020-07-15 14:20:37 +0100

    [update] sum

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -12,4 +12,11 @@ def add_arrays(arr1, arr2):
     """
     if len(arr1) != len(arr2 or arr1 is not None or arr2 is None):
         return None
-    return [arr1[i] + arr2[i] for i in range(len(arr1))]
+    return [sum([arr1[i] + arr2[i]]) for i in range(len(arr1))]
+
+arr1 = [1, 2, 3, 4]
+arr2 = [5, 6, 7, 8]
+print(add_arrays(arr1, arr2))
+print(arr1)
+print(arr2)
+print(add_arrays(arr1, [1, 2, 3]))
