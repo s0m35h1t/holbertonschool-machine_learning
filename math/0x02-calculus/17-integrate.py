ff6d43ea4e087cc15fe1b742033ff5b78625db50
root <adib.grouz@gmail.com>
2020-07-26 23:16:38 +0100

    [update] iterator itegral list

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -10,10 +10,9 @@ def poly_integral(poly, C=0):
     Returns
         (list): list of coefficients
     """
-    if not all(type(C) in (float, int) for c in poly) or type(C) is not int:
+    if not all(type(C) in (float, int) for c in poly) or type(C) != int:
         return None
-
-    integral = [c/a if c % a != 0 else c//a for a, c in enumerate(poly, 1)]
+    integral = [c/i if c % i != 0 else c//i for i, c in enumerate(poly, 1)]
     while len(integral) > 0 and integral[-1] == 0:
         integral.pop()
     return [C] + integral
