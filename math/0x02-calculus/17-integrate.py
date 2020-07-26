41fe82cca5469c39422ac41bbc5c4232b31c2c1a
root <adib.grouz@gmail.com>
2020-07-26 22:44:06 +0100

    [add] calculates integral of poly

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""calculates the integral of a polynomial"""
+
+
+def poly_integral(poly, C=0):
+    """calculates integral of a poly
+    Args
+        poly (list): polynomial list
+        C (int): representing the integration constant
+    Returns
+        (list): list of coefficients
+    """
+    if not all(type(C) in (float, int) for c in poly):
+        return None
+    if type(C) != int:
+        return
+    integral = [coeff/a if coeff % a != 0 else coeff //
+                a for a, coeff in enumerate(poly, 1)]
+    while len(integral) > 0 and integral[-1] == 0:
+        integral.pop()
+    return [C] + integral
