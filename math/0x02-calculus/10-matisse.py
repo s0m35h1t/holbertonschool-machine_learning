c016b73cf19c5fa13bc1033c82ab84c682e59c7f
root <adib.grouz@gmail.com>
2020-07-26 23:32:48 +0100

    [update] condition

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -9,7 +9,7 @@ def poly_derivative(poly):
     Returns:
         (list) list of coefficients, derivatives of polynomial
     """
-    if poly is None or not type(poly) is list:
+    if poly is None or type(poly) is not list:
         return None
     if len(poly) == 0 or type(poly[0]) is not int:
         return None
