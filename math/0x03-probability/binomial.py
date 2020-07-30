757c19dab22acabe0ffd6757aaff140e593d1085
root <adib.grouz@gmail.com>
2020-07-30 23:04:08 +0100

    [fix] sum cdf

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -62,5 +62,6 @@ class Binomial:
         """
         if k > self.n or k < 0:
             return 0
-        sum = sum([self.pmf(i) for i in range(0, int(k) + 1)])
-        return sum if sum else  0
+        sum = 0
+        [sum:= sum + self.pmf(i) for i in range(0, int(k) + 1)]
+        return sum
