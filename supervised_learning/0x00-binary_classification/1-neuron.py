bd636432671d6c194f7bd2fb2aa7953b4ae0ccf0
root <adib.grouz@gmail.com>
2020-08-08 15:16:12 +0100

    [update] weights vect init

--- supervised_learning/0x00-binary_classification/1-neuron.py
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -15,7 +15,7 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.W = np.ndarray.randn((1, nx))
+        self.__W = np.random.randn(1, nx))
         self.b = 0
         self.A = 0
 
