04dfdcb486566e88b1833c4ade61d13575d0313b
root <adib.grouz@gmail.com>
2020-08-09 14:52:49 +0100

    [update] for instead of while

--- supervised_learning/0x00-binary_classification/6-neuron.py
+++ supervised_learning/0x00-binary_classification/6-neuron.py
@@ -96,7 +96,7 @@ class Neuron:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
-        while iterations > 0:
-            self.gradient_descent(X, Y, self.forward_prop(X), alpha)
-            iterations -= 1
+        for i in range(iterations):
+            self.forward_prop(X)
+            self.gradient_descent(X, Y, self.__A, alpha)
         return self.evaluate(X, Y)
