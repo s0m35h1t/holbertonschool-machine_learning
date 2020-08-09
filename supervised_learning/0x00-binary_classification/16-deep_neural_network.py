36c18f1a4ababc5e6bc88d47ad3b8b46823175a3
root <adib.grouz@gmail.com>
2020-08-09 18:55:56 +0100

    [Add] Deep neural network class

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -14,23 +14,20 @@ class DeepNeuralNetwork:
             nx (int): number of input features
             layers (list): number of nodes in each layer of the network
         """
-        if type(nx) is not int:
-            raise TypeError("nx must be an integer")
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        if type(layers) is not list or len(layers) < 1:
+            raise ValueError("nx must be positive")
+        if not isinstance(layers, list):
             raise TypeError("layers must be a list of positive integers")
-        for layer in layers:
-            if type(layer) is not int or layer < 1:
-                raise TypeError("layers must be a list of positive integers")
+        if min(layers) < 1:
+            raise TypeError("layers must be a list of positive integers")
+
         self.L = len(layers)
         self.cache = {}
-        self.weights = {"W1": np.random.randn(layers[0], nx) *
-                        np.sqrt(2 / nx),
-                        "b1": np.zeros((layers[0], 1))}
-        for layer, size in enumerate(layers[1:], 2):
-            cur = "W" + str(layer)
-            self.weights[cur] = (np.random.randn(size, layers[layer - 2]) *
-                                 np.sqrt(2 / layers[layer - 2]))
-            cur = "b" + str(layer)
-            self.weights[cur] = np.zeros((layers[layer - 1], 1))
+        self.weights = {"W1": np.random.randn(
+            layers[0], nx), "b1": np.zeros((layers[0], 1))}
+        for i in range(1, self.L):
+            self.weights[("W{}".format(i+1))] = np.random.randn(layers[i],
+                                                                layers[i-1]) * np.sqrt(2/layers[i-1])
+            self.weights[("b{}".format(i+1))] = np.zeros((layers[i], 1))
