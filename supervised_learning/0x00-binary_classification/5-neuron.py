eadbabfa3fe90555e2f2b3ec790a5bc7c165009d
root <adib.grouz@gmail.com>
2020-08-09 14:12:43 +0100

    [update] formula gradient descent

--- supervised_learning/0x00-binary_classification/5-neuron.py
+++ supervised_learning/0x00-binary_classification/5-neuron.py
@@ -81,6 +81,6 @@ class Neuron:
         Returns:
             (None): Updates the private attributes __W and __b
         """
-        self.__W[0] = (self.__W[0] - alpha *
-                       np.dot(X, (A - Y).T).T[0] / X.shape[1])
-        self.__b -= alpha * (A[0] - Y[0]).mean()
+        self.__W = self.__W - \
+            (alpha * np.sum(X * (A - Y), axis=1) / X.shape[1])
+        self.__b = self.__b - (alpha * np.sum(A - Y) / X.shape[1])
