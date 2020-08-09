44dc2188301a8a35fe21964baa510364f6d41d40
root <adib.grouz@gmail.com>
2020-08-09 14:23:39 +0100

    [update] debug  script

--- supervised_learning/0x00-binary_classification/5-neuron.py
+++ supervised_learning/0x00-binary_classification/5-neuron.py
@@ -85,14 +85,3 @@ class Neuron:
             (alpha * np.sum(X * (A - Y), axis=1) / X.shape[1])
         self.__b = self.__b - (alpha * np.sum(A - Y) / X.shape[1])
 
-
-lib_train = np.load('Binary_Train.npz')
-X_3D, Y = lib_train['X'], lib_train['Y']
-X = X_3D.reshape((X_3D.shape[0], -1)).T
-
-np.random.seed(0)
-neuron = Neuron(X.shape[0])
-A = neuron.forward_prop(X)
-neuron.gradient_descent(X, Y, A, 0.5)
-print(neuron.W)
-print(neuron.b)
