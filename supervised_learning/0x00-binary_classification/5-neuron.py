dee5193dffe06f3daa63f49851ae13b08b643620
root <adib.grouz@gmail.com>
2020-08-09 15:21:06 +0100

    [update] init

--- supervised_learning/0x00-binary_classification/5-neuron.py
+++ supervised_learning/0x00-binary_classification/5-neuron.py
@@ -1,87 +1,56 @@
 #!/usr/bin/env python3
+""" Neuron class defining single neuron performing binary classification """
 
-"""Define: Neuron class => single neuron performing binary classification"""
 import numpy as np
 
 
 class Neuron:
-    """ Neuron class """
+    """ neuron class """
 
     def __init__(self, nx):
-        """Init Neuron
-        Args:
-            nx (int): number of input features to the neuron"""
+        """ nx: number of input features to the neuron """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.__W = np.random.randn(1, nx)
+        self.__W = np.ndarray((1, nx))
         self.__W[0] = np.random.normal(size=nx)
         self.__b = 0
         self.__A = 0
 
     @property
     def W(self):
-        """Returns weights"""
+        """ Returns weights """
         return self.__W
 
     @property
     def b(self):
-        """Returns bias"""
+        """ Returns bias """
         return self.__b
 
     @property
     def A(self):
-        """Returns activation values"""
+        """ Returns activation values """
         return self.__A
 
     def forward_prop(self, X):
-        """Calculates forward propagation of neuron
-        Args:
-            X (numpy.ndarray): nx, m) that contains the input data
-        Returns:
-            the private attribute __A
-        """
+        """ calculates forward propagation of neuron """
         self.__A = 1 / (1 + np.exp(-1 * (np.dot(self.__W, X) + self.__b)))
         return self.__A
 
     def cost(self, Y, A):
-        """Calculates cost of the model using logistic regression
-        Args:
-            Y (numpy.ndarray): (1, m) that contains the correct labels
-                                      for the input data
-            A (numpy.ndarray): (1, m) containing the activated output of
-                                      the neuron for each example
-        Returns:
-            the cost
-        """
+        """ calculates cost of the model using logistic regression """
+        costsum = 0
         return -(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)).mean()
 
     def evaluate(self, X, Y):
-        """Evalutes the neuron's predictions
-        Args:
-            X (numpy.ndarray ): (nx, m) that contains the input data
-            Y (numpy.ndarray ):  (1, m) that contains the correct
-                                labels for the input data
-        Returns:
-            the neuron s prediction and the cost of the network
-        """
+        """ Evalutes the neuron's predictions """
         A = np.ndarray((1, X.shape[1]))
         A[0] = self.forward_prop(X)
         return np.round(A).astype(int), self.cost(Y, A)
 
     def gradient_descent(self, X, Y, A, alpha=0.5):
-        """Calculates pass of gradient descent on the neuron
-        Args:
-            X (numpy.ndarray): (nx, m) that contains the input data
-            Y (numpy.ndarray): (1, m) that contains the correct labels
-                               for the input data
-            A (numpy.ndarray): (1, m) containing the activated output
-                                of the neuron for each example
-            Alpha (float): is the learning rate
-        Returns:
-            (None): Updates the private attributes __W and __b
-        """
+        """ Calculates one pass of gradient descent on the neuron """
         self.__W[0] = (self.__W[0] - alpha *
                        np.dot(X, (A - Y).T).T[0] / X.shape[1])
         self.__b -= alpha * (A[0] - Y[0]).mean()
