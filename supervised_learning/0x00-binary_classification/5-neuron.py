1755ed987f6a62cf242f1ad11f5c49a1f7226370
root <adib.grouz@gmail.com>
2020-08-09 15:14:50 +0100

    [update] formula & pep8

--- supervised_learning/0x00-binary_classification/5-neuron.py
+++ supervised_learning/0x00-binary_classification/5-neuron.py
@@ -81,6 +81,6 @@ class Neuron:
         Returns:
             (None): Updates the private attributes __W and __b
         """
-        self.__W[0] = (self.__W[0] - alpha *
+        self.__W = (self.__W - alpha *
                        np.dot(X, (A - Y).T).T[0] / X.shape[1])
         self.__b -= alpha * (A[0] - Y[0]).mean()
