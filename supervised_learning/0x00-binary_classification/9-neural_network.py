c2ff143335c6ccab8a60cba9b6ff46f556bcb407
root <adib.grouz@gmail.com>
2020-08-09 16:30:31 +0100

    [add] attrb propeerties

---
+++ supervised_learning/0x00-binary_classification/9-neural_network.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Define: Neural network class """
+
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """ neural network class """
+
+    def __init__(self, nx, nodes):
+        """ nx: number of input features """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.random.randn(nodes, nx)
+        """self.b1 = [[0] for i in [nodes] * nodes]"""
+        self.__b1 = np.zeros(shape=(nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def A2(self):
+        return self.__A2
