cad895d75a31b4bd1c599f28bc6459a69a758062
root <adib.grouz@gmail.com>
2020-08-09 19:00:22 +0100

    [Add] Deep neural network class

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -5,6 +5,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class DeepNeuralNetwork:
     """ deep neural network class """
 
@@ -24,7 +25,10 @@ class DeepNeuralNetwork:
             raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         self.cache = {}
-        self.weights = {"W1": np.random.randn(layers[0], nx), "b1": np.zeros((layers[0], 1))}
+        self.weights = {"W1": np.random.randn(
+            layers[0], nx), "b1": np.zeros((layers[0], 1))}
         for i in range(1, self.L):
-            self.weights[("W{}".format(i+1))] = np.random.randn(layers[i], layers[i-1]) * np.sqrt(2/layers[i-1])
+            l = np.random.randn(layers[i],
+                                layers[i-1]) * np.sqrt(2/layers[i-1])
+            self.weights[("W{}".format(i+1))] = l
             self.weights[("b{}".format(i+1))] = np.zeros((layers[i], 1))
