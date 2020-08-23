a72fc2bc55a1c1adeaa326e9edca920bd48b5ba4
root <adib.grouz@gmail.com>
2020-08-23 23:11:08 +0100

    [fix] pep8 style

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 
-""" training operation for a neural network in tensorflow using 
-the Adam optimization algorithm:r"""
+"""Training operation for a neural network
+in tensorflow using the Adam optimization algorithm:r"""
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """updates the learning rate using inverse
-    time decay in numpyk 
+    time decay in numpyk
     Args:
         alpha is the original learning rate
-        decay_rate is the weight used to determine the rate at which alpha will decay
-        global_step is the number of passes of gradient descent that have elapsed
-        decay_step is the number of passes of gradient descent that should occur
+        decay_rate is the weight used to determine
+            the rate at which alpha will decay
+        global_step is the number of passes of
+            gradient descent that have elapsed
+        decay_step is the number of passes
+            of gradient descent that should occur
             before alpha is decayed furthero
-    Returns: 
+    Returns:
         updated value for alphaa
     """
     return alpha / (1 + decay_rate * int(global_step / decay_step))
