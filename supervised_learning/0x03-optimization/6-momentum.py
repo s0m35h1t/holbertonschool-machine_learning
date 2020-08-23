a24206b8fbd4f300dfebe796babfb17f6bd95e5d
root <adib.grouz@gmail.com>
2020-08-23 17:51:50 +0100

    [fix] pep8

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
 Creates the training operation for a neural network
-in tensorflow using the gradient descent with momentum optimization algorithm
+in tensorflow using the gradient descent with momentum
+optimization algorithm
 """
 import tensorflow as tf
 
@@ -13,9 +14,10 @@ def create_momentum_op(loss, alpha, beta1):
     Args:
         loss is the loss of the network
         alpha is the learning rate
-        beta1 is the momentum weight            
+        beta1 is the momentum weight
     Returns:
         the momentum optimization operation
     """
 
-    return tf.train.MomentumOptimizer(learning_rate=alpha, momentum=beta1).minimize(loss)
+    return tf.train.MomentumOptimizer(learning_rate=alpha,
+                                      momentum=beta1).minimize(loss)
