7e76a317d02bd5f88a90ce5f10d46aae3c95c0a6
root <adib.grouz@gmail.com>
2020-08-23 17:24:29 +0100

    [fix] pep8

--- supervised_learning/0x03-optimization/0-norm_constants.py
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -8,7 +8,7 @@ import numpy as np
 def normalization_constants(X):
     """calculates the normalization
     Args:
-        X (numpy.ndarray): shape (m, nx) to normalize 
+        X (numpy.ndarray): shape (m, nx) to normalize
     Returns:
         the mean and variance of each feature
     """
