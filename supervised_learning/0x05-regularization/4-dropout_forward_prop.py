29fac6defb760ff7fe6f6c5c67c63d8fc31fcfe4
root <adib.grouz@gmail.com>
2020-08-28 23:27:59 +0100

    [Add] intrc

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -15,29 +15,29 @@ def dropout_forward_prop(X, weights, L, keep_prob):
             m: is the number of data points
         weights: dictionary of the weights and biases
         of the neural network
-        L: number of layers in the network
+        L: number of ls in the network
         keep_prob: probability that a node will be kept
-        All layers except the last should use the tanh activation function
+        All ls except the last should use the tanh activation function
     Returns:
         cache
     """
-    cache = {}  # dict that holds intermediate values of the network
+    cache = {}
     cache['A0'] = X
-    for layer in range(L):
-        W = weights["W" + str(layer + 1)]
-        A = cache["A" + str(layer)]
-        B = weights["b" + str(layer + 1)]
+    for l in range(L):
+        W = weights["W" + str(l + 1)]
+        A = cache["A" + str(l)]
+        B = weights["b" + str(l + 1)]
         Z = np.matmul(W, A) + B
         dropout = np.random.rand(Z.shape[0], Z.shape[1])
         dropout = np.where(dropout < keep_prob, 1, 0)
-        if layer == L - 1:
+        if l == L - 1:
             softmax = np.exp(Z)
-            cache["A" + str(layer + 1)] = (softmax / np.sum(softmax, axis=0,
+            cache["A" + str(l + 1)] = (softmax / np.sum(softmax, axis=0,
                                                             keepdims=True))
         else:
             tanh = np.tanh(Z)
-            cache["A" + str(layer + 1)] = tanh
-            cache["D" + str(layer + 1)] = dropout
-            cache["A" + str(layer + 1)] *= dropout
-            cache["A" + str(layer + 1)] /= keep_prob
+            cache["A" + str(l + 1)] = tanh
+            cache["D" + str(l + 1)] = dropout
+            cache["A" + str(l + 1)] *= dropout
+            cache["A" + str(l + 1)] /= keep_prob
     return cache
