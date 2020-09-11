e1ddec442087dbb563f76417335e1bafb839af6c
root <adib.grouz@gmail.com>
2020-09-11 22:56:38 +0100

    [fix] pep8

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -4,41 +4,56 @@
 import numpy as np
 
 
-def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
-    """Back propagation over a pooling layer in a CNN
+def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
+    """Backward propagate over a convolutional layer in a NN
     Args:
-        dA: numpy.ndarray of shape (m, h_new, w_new, c_new) containing
+        dZ: numpy.ndarray of shape (m, h_new, w_new, c_new) containing
             the partial derivatives with respect to the unactivated output of
             the convolutional layer
             m: the number of examples
             h_new: the height of the output
             w_new: the width of the output
-            c_new: the number of channels
+            c_new: the number of channels in the output
         A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing
                 the output of the previous layer
                 h_prev: the height of the previous layer
                 w_prev: the width of the previous layer
-        kernel_shape: tuple of (kh, kw) containing the size of the kernel for
-                      the pooling
-                      kh: the kernel height
-                      kw: the kernel width
+                c_prev: the number of channels in the previous layer
+        W: numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the
+           kernels for the convolution
+           kh: the filter height
+           kw: the filter width
+        b: numpy.ndarray of shape (1, 1, 1, c_new) containing the biases
+           applied to the convolution
+        padding: string that is either same or valid, indicating the type of
+                 padding used
         stride: tuple of (sh, sw) containing the strides for the convolution
                 sh: the stride for the height
                 sw: the stride for the width
-        mode: string containing either max or avg, indicating whether to
-              perform maximum or avg pooling, respectively
     Returns:
         the partial derivatives with respect to the previous layer
-        (dA_prev)
+        (dA_prev), the kernels (dW), and the biases (db), respectively
     """
     (m, h_prev, w_prev, c_prev) = A_prev.shape
-    (m, h_new, w_new, c_new) = dA.shape
-    (kh, kw) = kernel_shape
+    (m, h_new, w_new, c_new) = dZ.shape
+    (kh, kw, c_prev, c_new) = W.shape
     sh, sw = stride
+    if padding == 'same':
+        ph = int(np.ceil((((h_prev - 1) * sh + kh - h_prev) / 2)))
+        pw = int(np.ceil((((w_prev - 1) * sw + kw - w_prev) / 2)))
+    if padding == 'valid':
+        pw = 0
+        ph = 0
     dA_prev = np.zeros(A_prev.shape)
-
+    dW = np.zeros(W.shape)
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
+    A_prev_pad = np.pad(A_prev, pad_width=((0, 0), (ph, ph), (pw, pw),
+                                           (0, 0)), mode='constant')
+    dA_prev_pad = np.pad(dA_prev, pad_width=((0, 0), (ph, ph), (pw, pw),
+                                             (0, 0)), mode='constant')
     for i in range(m):
-        a_prev = A_prev[i]
+        a_prev_pad = A_prev_pad[i]
+        da_prev_pad = dA_prev_pad[i]
         for h in range(h_new):
             for w in range(w_new):
                 for c in range(c_new):
@@ -46,21 +61,15 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
                     v_end = v_beg + kh
                     h_start = w * sw
                     h_end = h_start + kw
+                    a_slice = a_prev_pad[v_beg:v_end, h_start:h_end]
+                    da_prev_pad[v_beg:v_end,
+                                h_start:h_end] += \
+                        W[:, :, :, c] * dZ[i, h, w, c]
+                    dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
 
-                    if mode == 'max':
-                        a_slice = a_prev[v_beg:v_end, h_start:h_end, c]
+        if padding == 'same':
+            dA_prev[i, :, :, :] += da_prev_pad[ph:-ph, pw:-pw, :]
+        if padding == 'valid':
+            dA_prev[i, :, :, :] += da_prev_pad
 
-                        mask = (a_slice == np.max(a_slice))
-                        dA_prev[i, v_beg:v_end,
-                                h_start:h_end,
-                                c] += np.multiply(mask, dA[i, h, w, c])
-
-                    elif mode == 'avg':
-                        da = dA[i, h, w, c]
-                        shape = kernel_shape
-                        avg = da / (kh * kw)
-                        Z = np.ones(shape) * avg
-                        dA_prev[i,
-                                v_beg:v_end,
-                                h_start:h_end, c] += Z
-    return dA_prev
+    return dA_prev, dW, db
