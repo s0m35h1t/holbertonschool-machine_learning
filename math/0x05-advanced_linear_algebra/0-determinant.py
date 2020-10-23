b6ded7835a2a170588013959a8657aec2b2d71c9
root <root@DESKTOP-3L7UPTA.localdomain>
2020-10-23 22:40:39 +0100

    [add] advs algebra

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Determinant of matrix
+"""
+
+
+def minor(m, row, col):
+    """Omite the the given row and column of a square matrix.
+    Args:
+        m (list): matrix.
+        row (int): row to omite.
+        col (int): column to omite.
+    Returns:
+        the matrix with the omited row, column.
+    """
+    return [[m[i][j] for j in range(len(m[i])) if j != col]
+            for i in range(len(m)) if i != row]
+
+
+def determinant(matrix):
+    """ Calculates the determinant of a square matrix.
+    Args:
+        matrix (list): matrix to calculate.
+    Returns:
+        the determinant of matrix
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if all([type(i) is list for i in matrix]) is False:
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix[0] and len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+
+    if matrix == [[]]:
+        return 1
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for j in range(len(matrix[0])):
+        om = minor(matrix, 0, j)
+        det += matrix[0][j] * ((-1) ** j) * determinant(om)
+
+    return det
---
+++ math/0x05-advanced_linear_algebra/3-adjugate.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+"""
+adjugate of matrix
+"""
+
+
+def minor(m, row, col):
+    """Omite the the given row and column of a square matrix.
+    Args:
+        m (list): matrix.
+        row (int): row to omite.
+        col (int): column to omite.
+    Returns:
+        the matrix with the omited row, column.
+    """
+    return [[m[i][j] for j in range(len(m[i])) if j != col]
+            for i in range(len(m)) if i != row]
+
+
+def determinant(matrix):
+    """ Calculates the determinant of a square matrix.
+    Args:
+        matrix (list): matrix to calculate.
+    Returns:
+        the determinant of matrix
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if all([type(i) is list for i in matrix]) is False:
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix[0] and len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+
+    if matrix == [[]]:
+        return 1
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for j in range(len(matrix[0])):
+        om = minor(matrix, 0, j)
+        det += matrix[0][j] * ((-1) ** j) * determinant(om)
+
+    return det
+
+def transponse(m):
+    """Calculates the transpose of a matrix
+    Args:
+        m (list): matrix.
+    Returns:
+        the transposed matrix
+    """
+    return [[m[row][col] for row in range(len(m))]
+            for col in range(len(m[0]))]
+
+
+def adjugate(matrix):
+    """ Calculates the adjugate of a square matrix.
+    Args:
+        matrix (list): matrix to calculate.
+    Returns:
+        the adjugate.
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if all([type(i) is list for i in matrix]) is False:
+        raise TypeError("matrix must be a list of lists")
+
+    if (len(matrix) == 0 or len(matrix) != len(matrix[0])) \
+            or matrix == [[]]:
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    if any([len(l) != len(matrix) for l in matrix]):
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    if len(matrix) == 1 and len(matrix[0]) == 1:
+        return [[1]]
+
+    cofactor = [[((-1) ** (i + j)) * determinant(minor(matrix, i, j))
+                 for j in range(len(matrix[i]))] for i in range(len(matrix))]
+
+    return transponse(cofactor)
