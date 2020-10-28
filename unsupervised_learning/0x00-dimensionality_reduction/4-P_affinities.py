2e2e479ced985335a64cf203c6feca22f400f3e8
root <root@DESKTOP-3L7UPTA.localdomain>
2020-10-28 20:38:04 +0100

    [Add] exe

--- unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -1,25 +1,52 @@
 #!/usr/bin/env python3
-"""Q affinities"""
+"""Shannon entropy and P affinities"""
 
 import numpy as np
+P_init = __import__('2-P_init').P_init
+HP = __import__('3-entropy').HP
 
 
-def Q_affinities(Y):
-    """Computes Q affinities
-    Args:
-        Y: (numpy.ndarray) of shape (n, ndim)
-           containing the low dimensional transformation of X
-    Returns: 
-        Q, num
-        Q: ((numpy.ndarray)) of shape (n, n) containing
-        the Q affinities
-        num: ((numpy.ndarray)) of shape (n, n) containing
-            the numerator of the Q affinities
+def P_affinities(X, tol=1e-5, perplexity=30.0):
     """
-    sum_Y = np.sum(np.square(Y), 1)
-    numerator = -2. * np.dot(Y, Y.T)
-    numerator = 1. / (1. + np.add(np.add(numerator, sum_Y).T, sum_Y))
-    np.fill_diagonal(numerator, 0)
-    Q = numerator / np.sum(numerator)
+    Binary search to get P affinities so each gaussian
+    has the same perplexity
+    Args:
+        X:
+        tol:
+        perplexity:
+    Returns:
+    """
+    (n, d) = X.shape
+    D, P, betas, H = P_init(X, perplexity)
 
-    return Q, numerator
+    if n == 0:
+        return P
+
+    for i in range(n):
+        row = D[i].copy()
+        row = np.delete(row, i, axis=0)
+        Hi, Pi = HP(row, betas[i])
+        Hdiff = Hi - H
+        b_max = None
+        b_min = None
+        while np.abs(Hdiff) > tol:
+            if Hdiff > 0:
+                b_min = betas[i, 0]
+                if b_max is None:
+                    betas[i, 0] = betas[i, 0] * 2.
+                else:
+                    betas[i, 0] = (betas[i, 0] + b_max) / 2.
+            else:
+                b_max = betas[i, 0]
+                if b_min is None:
+                    betas[i, 0] = betas[i, 0] / 2.
+                else:
+                    betas[i, 0] = (betas[i, 0] + b_min) / 2.
+
+            Hi, Pi = HP(row, betas[i])
+            Hdiff = Hi - H
+        Pi = np.insert(Pi, i, 0)
+        P[i] = Pi
+
+    P = (P.T + P) / (2 * n)
+    return P
---
+++ unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Q affinities"""
+
+import numpy as np
+
+
+def Q_affinities(Y):
+    """
+    Function that computes Q affinities
+    Args:
+        Y: numpy.ndarray of shape (n, ndim)
+           containing the low dimensional transformation of X
+    Returns: 
+        Q, num
+        Q: is a numpy.ndarray of shape (n, n) containing
+        the Q affinities
+        num: is a numpy.ndarray of shape (n, n) containing
+            the numerator of the Q affinities
+    """
+    sum_Y = np.sum(np.square(Y), 1)
+    numerator = -2. * np.dot(Y, Y.T)
+    numerator = 1. / (1. + np.add(np.add(numerator, sum_Y).T, sum_Y))
+    np.fill_diagonal(numerator, 0)
+    Q = numerator / np.sum(numerator)
+
+    return Q, numerator
