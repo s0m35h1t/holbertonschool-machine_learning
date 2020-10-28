a9f9479907b894d1ec352b2c3a921debad126495
root <root@DESKTOP-3L7UPTA.localdomain>
2020-10-28 20:47:00 +0100

    [Add] exe

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -18,9 +18,9 @@ def pca(X, var=0.95):
         original variance. W is a ndarray (d, nd)
         nd is the new dimensionality o the transformed X
     """
-    u, Sigma, vh = np.linalg.svd(X, full_matrices=False)
-    cumu = np.cumsum(Sigma) / np.sum(Sigma)
-    r = (np.argwhere(cumu >= var))[0, 0]
+    _, Sigma, vh = np.linalg.svd(X, full_matrices=False)
+    cumulative_var = np.cumsum(Sigma) / np.sum(Sigma)
+    r = (np.argwhere(cumulative_var >= var))[0, 0]
     w = vh.T
     wr = w[:, :r + 1]
     return wr
