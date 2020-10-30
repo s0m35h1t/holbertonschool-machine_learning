f5b9d4ead225e6545b1ccb67a61df477e8aaa595
root <root@DESKTOP-3L7UPTA.localdomain>
2020-10-30 22:06:57 +0100

    [add] bayesian_prob

---
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""Likelihood"""
+
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """Patient who takes this drug will develop
+    severe side effects.
+    Args:
+`        x follows a binomial distribution.
+        x is the number of patients that develop severe side effects
+        n is the total number of patients observed
+        P is a 1D numpy.ndarray containing the various hypothetical probabilities
+        of developing severe side effects`
+    Returns:
+        (numpy.ndarray) with the likelihood of obtaining the data,
+        x and n, for each probability in P, respectively
+    """
+
+    if not isinstance(n, (int, float)) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, (int, float)) or x < 0:
+        message = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(message)
+
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if np.any(P > 1) or np.any(P < 0):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+
+    com = np.math.factorial(n)/(np.math.factorial(x) * np.math.factorial(n-x))
+    lh = com * pow(P, x) * pow(1 - P, n - x)
+
+    return lh
