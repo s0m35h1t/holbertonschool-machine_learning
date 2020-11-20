36da5e17c849bcb2c6770572e2070fa0252d83f7
root <root@DESKTOP-3L7UPTA.localdomain>
2020-11-20 22:46:58 +0100

    [Fix] pepstyle

--- unsupervised_learning/0x03-hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/1-gp.py
@@ -41,10 +41,9 @@ class GaussianProcess:
         Returns:
             the covariance kernel matrix as a numpy.ndarray of shape (m, n)
         """
-        return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 *
-                                          (np.sum(X1 ** 2, 1).reshape(-1, 1)
-                                           + np.sum(X2 ** 2, 1)
-                                           - 2 * np.dot(X1, X2.T)))
+        li = np.sum(X1 ** 2, 1).reshape(-1, 1) + \
+            np.sum(X2 ** 2, 1) - 2 * np.dot(X1, X2.T)
+        return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * li))
 
     def predict(self, X_s):
         """predicts the mean and standard deviation of points
