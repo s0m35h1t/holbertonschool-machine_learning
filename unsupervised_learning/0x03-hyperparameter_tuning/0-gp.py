43dbc10f16ade6ae8d2c5529fc8d2542f57d7ec1
root <root@DESKTOP-3L7UPTA.localdomain>
2020-11-20 22:37:58 +0100

    [Fix] pepstyle

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -11,9 +11,11 @@ class GaussianProcess:
         """
         Class constructor: def __init__(self, X_init, Y_init, l=1, sigma_f=1):
             X_init is a numpy.ndarray of shape (t, 1)
-                representing the inputs already sampled with the black-box function
+                representing the inputs already sampled with the black-box
+                function
             Y_init is a numpy.ndarray of shape (t, 1)
-                representing the outputs of the black-box function for each input in X_init
+                representing the outputs of the black-box function
+                for each input in X_init
             t is the number of initial samples
             l is the length parameter for the kernel
             sigma_f is the standard deviation given to the output
@@ -39,7 +41,7 @@ class GaussianProcess:
         Returns:
             the covariance kernel matrix as a numpy.ndarray of shape (m, n)
         """
-        return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 *
-                                          (np.sum(X1 ** 2, 1).reshape(-1, 1)
-                                           + np.sum(X2 ** 2, 1)
-                                           - 2 * np.dot(X1, X2.T)))
+        return self.sigma_f ** 2 * \
+            np.exp(-0.5 / self.l ** 2 *
+                   (np.sum(X1 ** 2, 1).reshape(-1, 1)
+                    + np.sum(X2 ** 2, 1) - 2 * np.dot(X1, X2.T)))
