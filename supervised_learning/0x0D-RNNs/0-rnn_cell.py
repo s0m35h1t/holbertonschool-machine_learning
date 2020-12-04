19260ac195977e46728542fda5a5980e393d15df
root <root@DESKTOP-3L7UPTA.localdomain>
2020-12-04 19:21:16 +0100

    [Add] rnn

---
+++ supervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""RNN Cell"""
+import numpy as np
+
+
+class RNNCell:
+    """Represents a cell of a simple RNN"""
+
+    def __init__(self, i, h, o):
+        """Constructor Cell Init
+        Args:
+            i is the dimensionality of the data
+            h is the dimensionality of the hidden state
+            o is the dimensionality of the outputs
+            Creates the public instance attributes Wh, Wy, bh, by that
+            represent the weights and biases of the cell
+                Wh and bh are for the concatenated hidden state and input
+                data
+                Wy and by are for the output
+            The weights should be initialized using a random normal
+            distribution in the order listed above
+            The weights will be used on the right side for matrix
+            multiplication
+            The biases should be initialized as zeros
+        """
+        self.Wh = np.random.normal(0, 1, (i + h, h))
+        self.Wy = np.random.normal(0, 1, (h, o))
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """Performs forward propagation for one time step
+        Args:
+            x_t is a numpy.ndarray of shape (m, i) that contains the
+            data input for the cell
+                m is the batche size for the data
+            h_prev is a numpy.ndarray of shape (m, h) containing the
+            previous hidden state
+            The output of the cell should use a softmax activation function
+        Returns: h_next, y
+                h_next is the next hidden state
+                y is the output of the cell
+        """
+        h_next = np.tanh(np.dot(np.concatenate(
+            (h_prev, x_t), axis=1), self.Wh) + self.bh)
+        y = np.dot(h_next, self.Wy) + self.by
+        return h_next, np.exp(y) / np.exp(y).sum(axis=1, keepdims=True)
---
+++ supervised_learning/0x0D-RNNs/1-rnn.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""RNN"""
+import numpy as np
+
+
+def rnn(rnn_cell, X, h_0):
+    """Performs forward propagation for a simple RNN
+    Args:
+        X is the data to be used, given as a numpy.ndarray of
+          shape (t, m, i)
+            t is the maximum number of time steps
+            m is the batch size
+            i is the dimensionality of the data
+        h_0 is the initial hidden state, given as a numpy.ndarray
+          of shape (m, h)
+            h is the dimensionality of the hidden state
+    Returns: H, Y
+        H is a numpy.ndarray containing all of the hidden states
+        Y is a numpy.ndarray containing all of the outputs
+    """
+    T, m, _ = X.shape
+    h = h_0.shape[1]
+    H = np.zeros((T + 1, m, h))
+    Y = np.zeros((T, m, rnn_cell.Wy.shape[1]))
+
+    H[0] = h_0
+    for t in range(T):
+        H[t], Y[t - 1] = rnn_cell.forward(H[t - 1], X[t - 1])
+
+    return H, Y
