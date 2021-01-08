86e269f56f3e19df26e0d3caf9153beeb14075d7
root <root@aleix.localdomain>
2021-01-08 19:20:45 +0100

    [Add] pos encoding

--- supervised_learning/0x11-attention/4-positional_encoding.py
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -14,13 +14,13 @@ def positional_encoding(max_seq_len, dm):
     Returns: a numpy.ndarray of shape (max_seq_len, dm)
         containing the positional encoding vectorsr"""
 
-    angle_rates = 1 / \
+    angle = 1 / \
         np.power(
             10000, (2 * (np.arange(dm)[np.newaxis, :]//2)) / np.float32(dm))
-    angle_rads = np.arange(max_seq_len)[:, np.newaxis] * angle_rates
+    angle = np.arange(max_seq_len)[:, np.newaxis] * angle
 
-    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    angle[:, 0::2] = np.sin(angle[:, 0::2])
 
-    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    angle[:, 1::2] = np.cos(angle[:, 1::2])
 
-    return angle_rads
+    return angle
